8d2d92add29b63c508804d45b69ad3d74aba94a3
Lukas Lazarek <LukasALazarek@gmail.com>
2019-04-03 17:09:45 -0500

    info: Fix dependencies list

--- info.rkt
+++ info.rkt
@@ -3,4 +3,5 @@
 (define name "ruinit")
 (define version "0.01")
 (define scribblings '(("scribblings/ruinit-doc.scrbl" ())))
-(define deps '("rackunit" "syntax/parse" "syntax/to-string" "racket/pretty"))
+(define build-deps '("racket-doc" "scribble-lib"))
+(define deps '("base" "rackunit"))
